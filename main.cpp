6cabbb57db70a8502259bb3c3f351f7af52f9784
LorenzoBinosi <lorenzo.binosi@gmail.com>
2017-08-08 18:23:59 +0200

    Added parameters in the main function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 #include "Multiplication.h"
 #include "StandardTiling.h"
 #include "KaratsubaOfman.h"
@@ -8,48 +9,91 @@
 
 using namespace std;
 
-int main ()
+bool is_number(char*);
+
+int main (int argc, char** argv)
 {
     int i, j, delay;
     short input1, input2, minInput1, minInput2;
     long long in1, in2;
+    int inputLength1, inputLength2;
     MultiplicationTree *ptr;
     vector <Multiplier> multipliers;
     vector <Multiplication*> multiplications;
     vector <MultiplicationTree> multiplicationTrees;
-
     ifstream infile("multipliers");
 
-    //Creating multipliers
-    while (infile >> input1 >> input2 >> minInput1 >> minInput2 >> delay)
-    {	
-        try
-        {
-            multipliers.push_back(Multiplier (input1, input2, minInput1, minInput2, delay));		
-        }
-        catch (const invalid_argument& e)
-        {
-            cerr << e.what() << endl;
-        }
-    }
-    in1 = 5;
-    in2 = -5;
-    //Karatsuba-Ofman multiplication;
-    multiplications.push_back(new KaratsubaOfman(multipliers));
-    //Standard Tiling
-    multiplications.push_back(new StandardTiling(multipliers));
-    //Proposed Tiling
-    multiplications.push_back(new ProposedTiling(multipliers));
-    for (j = 0; j < multiplications.size(); j++)
+    if (argc == 5 && is_number(argv[1]) && is_number(argv[2]) && is_number(argv[3]) && is_number(argv[4]))
     {
-        multiplicationTrees = (multiplications[j])->dispositions(32, 32);
-        for (i = 0; i < multiplicationTrees.size(); i++)
+        in1 = atoll(argv[3]);
+        in2 = atoll(argv[4]);
+        inputLength1 = atoi(argv[1]);
+        inputLength2 = atoi(argv[2]);
+        //Creating multipliers
+        while (infile >> input1 >> input2 >> minInput1 >> minInput2 >> delay)
+        {	
+            try
+            {
+                multipliers.push_back(Multiplier (input1, input2, minInput1, minInput2, delay));		
+            }
+            catch (const invalid_argument& e)
+            {
+                cerr << e.what() << endl;
+            }
+        }
+        //Karatsuba-Ofman multiplication;
+        multiplications.push_back(new KaratsubaOfman(multipliers));
+        //Standard Tiling
+        multiplications.push_back(new StandardTiling(multipliers));
+        //Proposed Tiling
+        multiplications.push_back(new ProposedTiling(multipliers));
+        for (j = 0; j < multiplications.size(); j++)
         {
-            cout << multiplicationTrees[i].getDescription() << endl;
-            cout << "Expression: " << multiplicationTrees[i].getExpression() << endl;
-            cout << "Delay: " << multiplicationTrees[i].getDelay() << endl;
-            cout << "Expected value: " << (in1 * in2) << endl;
-            cout << "Obtained value: " << multiplicationTrees[i].executeMultiplication(in1, in2) << endl;
+            multiplicationTrees = (multiplications[j])->dispositions(inputLength1, inputLength2);
+            for (i = 0; i < multiplicationTrees.size(); i++)
+            {
+                cout << multiplicationTrees[i].getDescription() << endl;
+                cout << "Expression: " << multiplicationTrees[i].getExpression() << endl;
+                cout << "Delay: " << multiplicationTrees[i].getDelay() << endl;
+                cout << "Expected value: " << (in1 * in2) << endl;
+                cout << "Obtained value: " << multiplicationTrees[i].executeMultiplication(in1, in2) << endl;
+            }
         }
     }
+    else
+    {
+        cout << "usage: <input length 1> <input length 2> <first factor> <second factor>" << endl;
+    }
+}
+
+bool is_number(char* s)
+{
+    bool isNumber;
+    int i;
+
+    if (s[0] == '\0')
+    {
+        return false;
+    }
+    if (s[0] == 45)
+    {
+        i = 1;
+    }
+    else
+    {
+        i = 0;
+    }
+    if (s[i] == '\0')
+    {
+        return false;
+    }
+    isNumber = true;
+    for (; s[i] != '\0' && isNumber == true; i++)
+    {
+        if (s[i] < 48 || s[i] > 57)
+        {
+            isNumber = false;
+        }
+    }
+    return isNumber;
 }
